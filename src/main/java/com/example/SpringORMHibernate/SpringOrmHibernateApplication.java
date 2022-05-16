2ae5226a5c7d463c26b7ebaa467efa79d92714fb
YUGESH-AE <yugeshae@gmail.com>
2022-05-16 11:01:20 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringORMHibernate/SpringOrmHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringORMHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOrmHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOrmHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringORMHibernate/SpringOrmHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringORMHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOrmHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
